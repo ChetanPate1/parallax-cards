2db18a82b688aae44021f901f1d46f6e5b08fc1c
ChetanPate1 <=>
2017-04-13 15:52:37 +0100

    license add, readme updated, sass mixin add

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,54 +3,66 @@ module.exports = function( grunt ) {
 	grunt.initConfig( {
 
 		// Import package manifest
-		pkg: grunt.file.readJSON( "package.json" ),
+		pkg: grunt.file.readJSON( 'package.json' ),
 
 		// Banner definitions
 		meta: {
-			banner: "/*\n" +
-				" *  <%= pkg.title || pkg.name %> - v<%= pkg.version %>\n" +
-				" *  <%= pkg.description %>\n" +
-				" *  <%= pkg.homepage %>\n" +
-				" *\n" +
-				" *  Made by <%= pkg.author.name %>\n" +
-				" *  Under <%= pkg.license %> License\n" +
-				" */\n"
+			banner: '/*\n' +
+				' *  <%= pkg.title || pkg.name %> - v<%= pkg.version %>\n' +
+				' *  <%= pkg.description %>\n' +
+				' *  <%= pkg.homepage %>\n' +
+				' *\n' +
+				' *  Made by <%= pkg.author.name %>\n' +
+				' *  Under <%= pkg.license %> License\n' +
+				' */\n'
 		},
 
 		// Concat definitions
 		concat: {
 			options: {
-				banner: "<%= meta.banner %>"
+				banner: '<%= meta.banner %>'
 			},
 			dist: {
-				src: [ "src/<%= pkg.name %>.js" ],
-				dest: "dist/<%= pkg.name %>.js"
+				src: [ 'src/<%= pkg.name %>.js' ],
+				dest: 'dist/<%= pkg.name %>.js'
 			}
 		},
 
 		// Lint definitions
 		jshint: {
-			files: [ "src/<%= pkg.name %>.js", "test/**/*" ],
+			files: [ 'src/<%= pkg.name %>.js', 'test/**/*' ],
 			options: {
-				jshintrc: ".jshintrc"
+				jshintrc: '.jshintrc'
 			}
 		},
 
 		jscs: {
-			src: "src/**/*.js",
+			src: 'src/**/*.js',
 			options: {
-				config: ".jscsrc"
+				config: '.jscsrc'
 			}
 		},
 
 		// Minify definitions
 		uglify: {
 			dist: {
-				src: [ "dist/<%= pkg.name %>.js" ],
-				dest: "dist/<%= pkg.name %>.min.js"
+				src: [ 'dist/<%= pkg.name %>.js' ],
+				dest: 'dist/<%= pkg.name %>.min.js'
 			},
 			options: {
-				banner: "<%= meta.banner %>"
+				banner: '<%= meta.banner %>'
+			}
+		},
+
+		//SASS Compile
+		sass: {
+			dist: {
+				options:{
+					sourcemap: 'none'
+				},
+				files: {
+					'dist/parallax-cards.css': 'src/parallax-cards.scss'
+				}
 			}
 		},
 
@@ -58,7 +70,7 @@ module.exports = function( grunt ) {
 		coffee: {
 			compile: {
 				files: {
-					"dist/<%= pkg.name %>.js": "src/<%= pkg.name %>.coffee"
+					'dist/<%= pkg.name %>.js': 'src/<%= pkg.name %>.coffee'
 				}
 			}
 		},
@@ -66,17 +78,17 @@ module.exports = function( grunt ) {
 		// karma test runner
 		karma: {
 			unit: {
-				configFile: "karma.conf.js",
+				configFile: 'karma.conf.js',
 				background: true,
 				singleRun: false,
-				browsers: [ "Chrome" ]
+				browsers: [ 'Chrome' ]
 			},
 
 			//continuous integration mode: run tests once in PhantomJS browser.
 			travis: {
-				configFile: "karma.conf.js",
+				configFile: 'karma.conf.js',
 				singleRun: true,
-				browsers: [ "Chrome" ]
+				browsers: [ 'Chrome' ]
 			}
 		},
 
@@ -84,22 +96,23 @@ module.exports = function( grunt ) {
 		// Better than calling grunt a million times
 		// (call 'grunt watch')
 		watch: {
-			files: [ "src/*", "test/**/*" ],
-			tasks: [ "default" ]
+			files: [ 'src/*', 'test/**/*' ],
+			tasks: [ 'default' ]
 		}
 
 	} );
 
-	grunt.loadNpmTasks( "grunt-contrib-concat" );
-	grunt.loadNpmTasks( "grunt-contrib-jshint" );
-	grunt.loadNpmTasks( "grunt-jscs" );
-	grunt.loadNpmTasks( "grunt-contrib-uglify" );
-	grunt.loadNpmTasks( "grunt-contrib-coffee" );
-	grunt.loadNpmTasks( "grunt-contrib-watch" );
-	grunt.loadNpmTasks( "grunt-karma" );
+	grunt.loadNpmTasks( 'grunt-contrib-sass' );
+	grunt.loadNpmTasks( 'grunt-contrib-concat' );
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-jscs' );
+	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+	grunt.loadNpmTasks( 'grunt-contrib-coffee' );
+	grunt.loadNpmTasks( 'grunt-contrib-watch' );
+	grunt.loadNpmTasks( 'grunt-karma' );
 
-	grunt.registerTask( "travis", [ "jshint", "karma:travis" ] );
-	grunt.registerTask( "lint", [ "jshint", "jscs" ] );
-	grunt.registerTask( "build", [ "concat", "uglify" ] );
-	grunt.registerTask( "default", [ "jshint", "build", "karma:unit:run" ] );
+	grunt.registerTask( 'travis', [ 'jshint', 'karma:travis' ] );
+	grunt.registerTask( 'lint', [ 'jshint', 'jscs' ] );
+	grunt.registerTask( 'build', [ 'concat', 'uglify', 'sass' ] );
+	grunt.registerTask( 'default', [ 'jshint', 'build', 'sass', 'karma:unit:run' ] );
 };
--- README.md
+++ README.md
@@ -1,8 +1,53 @@
 # Parallax Cards
+Demos speak louder than words: In Progress
+<!-- [View]() -->
 
-<!-- ## Contributing -->
+## Motivation
+I was inspired by Apple TVs UI, the way the user can swipe over the remote to shimmer the card that was selected.
+This micro animation is subtle but adds so much more depth and user interaction to the web.
+
+## Usage
+```html
+<link rel="stylesheet" href="/dist/parallax-cards.css" />
+```
+
+### HTML Markup
+```html
+<div class="card-container">
+  <div class="card"> Your Content </div>
+</div>
+```
+or customise HTML markup names with dist/parallax-cards.scss
+
+### CSS
+**NOTE:** Create your own CSS, add translateZ on what elements you want closer or further eg.
+```css
+.card h1 {
+   transform: translateZ(50px);
+}
+```
+
+### Scripts
+```javascript
+<script src="/node_modules/jquery/dist/jquery.js"></script>
+```
+```javascript
+<script src="/dist/parallax-cards.js"></script>
+```
+
+### Initialize
+```javascript
+$('.card').parallaxCards(settings);
+```
+
+### Settings
+How exteme the rotation
+```javascript
+var settings = {
+	tolerance: 30 //between 5 - 100
+}
+```
 
 ## License
-
-<!-- [MIT License](http://zenorocha.mit-license.org/) -->
+[MIT License](https://github.com/ChetanPate1/parallax-cards/blob/master/LICENCE.md)
 © Chetan Patel
